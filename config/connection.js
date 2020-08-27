360d8d9dda74ffb01000f41c043b931708697382
karynclarke <karynclarke@comcast.net>
2020-08-27 13:05:38 -0700

    connection jaws

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 var mysql = require("mysql");
 require("dotenv").config()
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.MYSQLPASSWORD,
-    database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.MYSQLPASSWORD,
+        database: "burgers_db",
+    });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+    connection.connect(function(err) {
+        if (err) {
+            console.error("error connecting: " + err.stack);
+            return;
+        }
+        console.log("connected as id " + connection.threadId);
+    });
 
-module.exports = connection;
+    module.exports = connection;
